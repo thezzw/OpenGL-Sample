765a5c6c5908856bfb0a8d6acb56aea4c7e7b371
unknown <2117674050@qq.com>
2022-09-07 18:22:18 +0800

    Instanced array.

--- assets/shader/instanced.vert
+++ assets/shader/instanced.vert
@@ -1,14 +1,13 @@
 #version 330 core
 layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aOffset;
 
 out vec3 fColor;
 
-uniform vec2 offsets[100];
-
 void main()
 {
-	vec2 offset = offsets[gl_InstanceID];
-	gl_Position = vec4(aPos + offset, 0.0, 1.0);
-	fColor = aColor;
+    vec2 pos = aPos * (gl_InstanceID / 100.0);
+    gl_Position = vec4(pos + aOffset, 0.0, 1.0);
+    fColor = aColor;
 }
--- src/instanced.cpp
+++ src/instanced.cpp
@@ -61,7 +61,6 @@ int main()
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-	glBindVertexArray(0);
 
 	glm::vec2 translations[100];
 	int index = 0;
@@ -76,6 +75,19 @@ int main()
 			translations[index++] = translation;
 		}
 	}
+
+	unsigned int instanceVBO;
+	glGenBuffers(1, &instanceVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribDivisor(2, 1);
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
